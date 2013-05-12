5a4944abc8198e3652af2286a25ac89d0b80abc8
Mara <Mara@2e255eec-f6be-1440-8491-188ebe3a1c74>
2013-05-12 07:23:42 +0000

    WebUI project

---
+++ trunk/Pisqorks.WebUI/Controllers/ContentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pisqorks.WebUI.Controllers
+{
+    public class ContentController : Controller
+    {
+        //
+        // GET: /Content/
+
+        public string About()
+        {
+            return "About Pisqorks game!";
+        }
+
+    }
+}
